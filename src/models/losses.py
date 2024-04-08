dda9796c6c24d710f69c5c03b9e0e79e7e9a379c
IGragon <super1igor@gmail.com>
2024-04-08 16:04:17 +0300

    Added ArcFace and [arcface, sphereface, cosface] losses from other repos, we'll see if it works

--- src/models/losses.py
+++ src/models/losses.py
@@ -5,6 +5,8 @@ from typing import Union
 import torch
 import torch.nn as nn
 from torch.nn import TripletMarginWithDistanceLoss
+import torch.nn.functional as F
+import math
 
 
 class ContrastiveLoss(nn.Module):
@@ -17,10 +19,97 @@ class ContrastiveLoss(nn.Module):
         return self.H(predicted_sim / T, gt_sim)
 
 
-class ArcFace(nn.Module):
+class ArcFace(torch.nn.Module):
+    """ ArcFace
     # https://arxiv.org/pdf/1801.07698.pdf
     # https://github.com/deepinsight/insightface
-    pass
+    """
+    def __init__(self, s=64.0, margin=0.5):
+        super(ArcFace, self).__init__()
+        self.s = s
+        self.margin = margin
+        self.cos_m = math.cos(margin)
+        self.sin_m = math.sin(margin)
+        self.theta = math.cos(math.pi - margin)
+        self.sinmm = math.sin(math.pi - margin) * margin
+        self.easy_margin = False
+
+    def forward(self, logits: torch.Tensor, labels: torch.Tensor):
+        index = torch.where(labels != -1)[0]
+        target_logit = logits[index, labels[index].view(-1)]
+
+        with torch.no_grad():
+            target_logit.arccos_()
+            logits.arccos_()
+            final_target_logit = target_logit + self.margin
+            logits[index, labels[index].view(-1)] = final_target_logit
+            logits.cos_()
+        logits = logits * self.s
+        return logits
 
 
 # https://arxiv.org/pdf/1901.05903.pdf
+# https://github.com/cvqluu/Angular-Penalty-Softmax-Losses-Pytorch
+class AngularPenaltySMLoss(nn.Module):
+
+    def __init__(self, in_features, out_features, loss_type='arcface', eps=1e-7, s=None, m=None):
+        '''
+        Angular Penalty Softmax Loss
+
+        Three 'loss_types' available: ['arcface', 'sphereface', 'cosface']
+        These losses are described in the following papers:
+
+        ArcFace: https://arxiv.org/abs/1801.07698
+        SphereFace: https://arxiv.org/abs/1704.08063
+        CosFace/Ad Margin: https://arxiv.org/abs/1801.05599
+
+        '''
+        super(AngularPenaltySMLoss, self).__init__()
+        loss_type = loss_type.lower()
+        assert loss_type in ['arcface', 'sphereface', 'cosface']
+        if loss_type == 'arcface':
+            self.s = 64.0 if not s else s
+            self.m = 0.5 if not m else m
+        elif loss_type == 'sphereface':
+            self.s = 64.0 if not s else s
+            self.m = 1.35 if not m else m
+        elif loss_type == 'cosface':
+            self.s = 30.0 if not s else s
+            self.m = 0.4 if not m else m
+        else:
+            raise NotImplementedError(f'loss_type [{self.loss_type}] is not implemented!')
+        self.loss_type = loss_type
+        self.in_features = in_features
+        self.out_features = out_features
+        self.fc = nn.Linear(in_features, out_features, bias=False)
+        self.eps = eps
+
+    def forward(self, x, labels):
+        '''
+        input shape (N, in_features)
+        '''
+        assert len(x) == len(labels)
+        assert torch.min(labels) >= 0
+        assert torch.max(labels) < self.out_features
+
+        for W in self.fc.parameters():
+            W = F.normalize(W, p=2, dim=1)
+
+        x = F.normalize(x, p=2, dim=1)
+
+        wf = self.fc(x)
+        if self.loss_type == 'cosface':
+            numerator = self.s * (torch.diagonal(wf.transpose(0, 1)[labels]) - self.m)
+        elif self.loss_type == 'arcface':
+            numerator = self.s * torch.cos(torch.acos(
+                torch.clamp(torch.diagonal(wf.transpose(0, 1)[labels]), -1. + self.eps, 1 - self.eps)) + self.m)
+        elif self.loss_type == 'sphereface':
+            numerator = self.s * torch.cos(self.m * torch.acos(
+                torch.clamp(torch.diagonal(wf.transpose(0, 1)[labels]), -1. + self.eps, 1 - self.eps)))
+        else:
+            raise NotImplementedError(f'loss_type [{self.loss_type}] is not implemented!')
+
+        excl = torch.cat([torch.cat((wf[i, :y], wf[i, y + 1:])).unsqueeze(0) for i, y in enumerate(labels)], dim=0)
+        denominator = torch.exp(numerator) + torch.sum(torch.exp(self.s * excl), dim=1)
+        L = numerator - torch.log(denominator)
+        return -torch.mean(L)
