e749c6a953451731de351e328e885916ccedd092
IGragon <super1igor@gmail.com>
2024-04-10 15:25:45 +0300

    add KL-divergence metric (and its variations)

--- src/metrics.py
+++ src/metrics.py
@@ -1,19 +1,48 @@
 from tqdm import tqdm
 from sklearn.metrics import accuracy_score
+from scipy.special import kl_div as scipy_kl_div
 from scipy.spatial.distance import cosine
 import numpy as np
 
 
-def tpr_at_fpr(y_true, embeddings, fpr=0.01) -> float:
-    same_similarities = get_same_similarities(y_true, embeddings)
-    false_similarities = get_false_similarities(y_true, embeddings)
+def kl_div(y_true, embeddings, symmetry=None, verbose=False):
+    '''
+
+    :param y_true:
+    :param embeddings:
+    :param symmetry: default=None, can be 'jeffreys' or 'jensen-shannon'
+    :return:
+    '''
+    same_similarities = get_same_similarities(y_true, embeddings, verbose=verbose)
+    false_similarities = get_false_similarities(y_true, embeddings, verbose=verbose)
+
+    bins = np.linspace(-1, 1, 100)
+    same_sim_distr, _ = np.histogram(same_similarities, bins, density=True)
+    same_sim_distr += 1e-6
+    false_sim_distr, _ = np.histogram(false_similarities, bins, density=True)
+    false_sim_distr += 1e-6
+
+    if symmetry == 'jeffreys':
+        result = (scipy_kl_div(same_sim_distr, false_sim_distr) + scipy_kl_div(false_sim_distr, same_sim_distr)) / 2
+    elif symmetry == 'jensen-shannon':
+        m = (same_sim_distr + false_sim_distr) / 2
+        result = (scipy_kl_div(same_sim_distr, m) + scipy_kl_div(false_sim_distr, m)) / 2
+    else:
+        result = scipy_kl_div(same_sim_distr, false_sim_distr)
+
+    return sum(result)
+
+
+def tpr_at_fpr(y_true, embeddings, fpr=0.01, verbose=False) -> float:
+    same_similarities = get_same_similarities(y_true, embeddings, verbose=verbose)
+    false_similarities = get_false_similarities(y_true, embeddings, verbose=verbose)
 
     threshold_similarity = np.quantile(false_similarities, q=1-fpr)
     tpr = sum(same_similarities >= threshold_similarity) / len(same_similarities)
     return tpr
 
 
-def get_same_similarities(y_true, embeddings) -> np.ndarray:
+def get_same_similarities(y_true, embeddings, verbose=False) -> np.ndarray:
     '''
 
     :param y_true: (N,)
@@ -22,7 +51,11 @@ def get_same_similarities(y_true, embeddings) -> np.ndarray:
     '''
     unique_ids = np.unique(y_true)
     same_similarities = []
-    for id_ in tqdm(unique_ids, desc=f"Getting same similarities"):
+
+    pbar = unique_ids
+    if verbose:
+        pbar = tqdm(pbar, desc=f"Getting same similarities")
+    for id_ in pbar:
         current_embeddings = embeddings[y_true == id_, :]
         for e1 in current_embeddings:
             for e2 in current_embeddings:
@@ -32,7 +65,7 @@ def get_same_similarities(y_true, embeddings) -> np.ndarray:
     return np.array(same_similarities)
 
 
-def get_false_similarities(y_true, embeddings) -> np.ndarray:
+def get_false_similarities(y_true, embeddings, verbose=False) -> np.ndarray:
     '''
 
     :param y_true: (N,)
@@ -40,11 +73,14 @@ def get_false_similarities(y_true, embeddings) -> np.ndarray:
     :return: similarity scores of the false ids
     '''
     false_similarities = []
-    for id1, e1 in tqdm(zip(y_true, embeddings), desc=f"Getting false similarities"):
+
+    pbar = zip(y_true, embeddings)
+    if verbose:
+        pbar = tqdm(pbar, desc=f"Getting false similarities")
+    for id1, e1 in pbar:
         for id2, e2 in zip(y_true, embeddings):
             if id1 != id2:
                 false_similarities.append(cosine(e1, e2))
 
     return np.array(false_similarities)
-
 
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -3,7 +3,7 @@ import numpy as np
 from src import metrics
 
 n_classes = 10
-emb_dim = 128
+emb_dim = 32
 n_samples = 1000
 
 y_true = np.random.randint(0, n_classes + 1, size=(n_samples,))
@@ -15,3 +15,7 @@ embeddings = np.random.random((n_samples, emb_dim))
 fprs = [0.5, 0.2, 0.01]
 for fpr in fprs:
     print(f"TPR@FPR={fpr}: {metrics.tpr_at_fpr(y_true, embeddings, fpr)}")
+
+symmetries = [None, 'jeffreys', 'jensen-shannon']
+for symmetry in symmetries:
+    print(f"KL-Divergence ({symmetry=}): {metrics.kl_div(y_true, embeddings, symmetry=symmetry)}")
